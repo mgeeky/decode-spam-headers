78555219232b2fa32c99715cae4d284c808e7636
Max Ulidtko <ulidtko@gmail.com>
2025-12-16 14:01:41 +0100

    Fix parsing crash on some Received: chains
    Example crash:
    
    > Traceback (most recent call last):
    >   File "/tmp/decode-spam-headers.py", line 6925, in <module>
    >     main(sys.argv)
    >     ~~~~^^^^^^^^^^
    >   File "/tmp/decode-spam-headers.py", line 6905, in main
    >     out = an.parse(text)
    >   File "/tmp/decode-spam-headers.py", line 2285, in parse
    >     self.results[testName] = testFunc()
    >                              ~~~~~~~~^^
    >   File "/tmp/decode-spam-headers.py", line 5209, in testReceived
    >     vers = SMTPHeadersAnalysis.parseExchangeVersion(obj['ver'])
    >   File "/tmp/decode-spam-headers.py", line 2107, in parseExchangeVersion
    >     lookupparsed = packaging.version.parse(lookup)
    >   File "/Users/maksym.ivanov/Library/Python/3.14/lib/python/site-packages/packaging/version.py", line 56, in parse
    >     return Version(version)
    >   File "/Users/maksym.ivanov/Library/Python/3.14/lib/python/site-packages/packaging/version.py", line 202, in __init__
    >     raise InvalidVersion(f"Invalid version: {version!r}")
    > packaging.version.InvalidVersion: Invalid version: 'ffacd0b85a97d-42f7cbd8c20si11690432f8f.269.2025.12.09.06.21.40'
    
    In Recieved: headers, value under `id` is not necessarily formatted as a version string.

--- decode-spam-headers.py
+++ decode-spam-headers.py
@@ -130,6 +130,7 @@ import time
 import atexit
 import base64
 
+from bisect import bisect
 from html import escape
 from email import header as emailheader
 from datetime import *
@@ -2095,7 +2096,7 @@ class SMTPHeadersAnalysis:
             pass
 
         return ''
-            
+
     @staticmethod
     def parseExchangeVersion(lookup):
 
@@ -2104,34 +2105,23 @@ class SMTPHeadersAnalysis:
             if ver.version == lookup:
                 return ver
 
-        lookupparsed = packaging.version.parse(lookup)
-
         # Go with version-wise comparison to fuzzily find proper version name
-        sortedversions = sorted(SMTPHeadersAnalysis.Exchange_Versions)
-
-        match = re.search(r'\d{1,}\.\d{1,}\.\d{1,}', lookup, re.I)
-        if not match:
+        try:
+            lookupparsed = packaging.version.parse(lookup)
+        except packaging.version.InvalidVersion:
             return None
 
-        for i in range(len(sortedversions)):
-            if sortedversions[i].version.startswith(lookup):
-                sortedversions[i].name = 'fuzzy match: ' + sortedversions[i].name
-                return sortedversions[i]
+        sortedversions = sorted(SMTPHeadersAnalysis.Exchange_Versions)
 
-        for i in range(len(sortedversions)):
-            prevver = packaging.version.parse('0.0')
-            nextver = packaging.version.parse('99999.0')
-            if i > 0:
-                prevver = packaging.version.parse(sortedversions[i-1].version)
-            thisver = packaging.version.parse(sortedversions[i].version)
-            if i + 1 < len(sortedversions):
-                nextver = packaging.version.parse(sortedversions[i+1].version)
+        for v in sortedversions:
+            if v.version.startswith(lookup):
+                v.name = 'fuzzy match: ' + v.name
+                return v
 
-            if lookupparsed >= thisver and lookupparsed < nextver:
-                sortedversions[i].name = 'fuzzy match: ' + sortedversions[i].name
-                return sortedversions[i]
-
-        return None
+        inspos = bisect(sortedversions, lookupparsed)
+        sortedversions[inspos].name = 'fuzzy match: {} ({})'.format(
+                sortedversions[inspos].name, lookup)
+        return sortedversions[inspos]
 
 
     def getHeader(self, _header):
